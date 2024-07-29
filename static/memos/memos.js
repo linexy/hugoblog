6d29f0b9b3ab8925896884249ee37f17d9a165e6
linexy <linexy@qq.com>
2024-07-29 16:54:39 +0800

    up

--- static/memos/memos.js
+++ static/memos/memos.js
@@ -227,7 +227,7 @@ function updateHTMl(data,mode){
         <div class="memos_diaoyong_top">
         <div class="memos-tag-wz">${memosTag}</div>`
 
-      if(memoVis == "PUBLIC"){
+/*       if(memoVis == "PUBLIC"){
         result += `<div class="talks_comments">
             <a onclick="loadArtalk('${memo_id}')"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M12 11.25a.75.75 0 1 0 0 1.5a.75.75 0 0 0 0-1.5m-3 0a.75.75 0 1 0 0 1.5a.75.75 0 0 0 0-1.5m6 0a.75.75 0 1 0 0 1.5a.75.75 0 0 0 0-1.5m4.415-5.96C15.71 1.195 9.385.88 5.29 4.584C1.195 8.289.88 14.614 4.584 18.709l-2.438 2.437A.5.5 0 0 0 2.5 22H12a10 10 0 0 0 6.709-2.585c4.096-3.705 4.412-10.03.706-14.125M12 21H3.707l1.929-1.929a.5.5 0 0 0 0-.707a8.999 8.999 0 0 1 6.362-15.362A8.999 8.999 0 0 1 12 21"/></svg><span id="btn_memo_${memo_id}"></span></a>
           </div>
@@ -238,7 +238,7 @@ function updateHTMl(data,mode){
         result += `<div class="memos-hide" onclick="reloadList("NOPUBLIC")"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 14 14"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M1.68 4.206C2.652 6.015 4.67 7.258 7 7.258c2.331 0 4.348-1.243 5.322-3.052M2.75 5.596L.5 7.481m4.916-.415L4.333 9.794m6.917-4.198l2.25 1.885m-4.92-.415l1.083 2.728"/></svg></idv></div></li>`;  
       }else{
         result += `</div></li>`;
-      }        
+      } */        
   } // end for
   if(mode == "ONEDAY"){
     var bbBefore = "<li class='memos-oneday'><ul class='bb-list-ul'>";
